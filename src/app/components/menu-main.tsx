661187a8c54ee6582dd39fc3f477f3e6e8c80d9e
uttamapaksa <uttamapaksa@gmail.com>
2024-11-07 02:15:00 +0900

    test: action workflow-uttamapaksa blog deploy

--- src/app/components/menu-main.tsx
+++ src/app/components/menu-main.tsx
@@ -7,9 +7,9 @@ export default function MenuMain({ menu }: { menu: string }) {
         </h2>
       </header>
       <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        <div>aaa</div>
-        <div>bbb</div>
-        <div>ccc</div>
+        <div>aaaa</div>
+        <div>bbbb</div>
+        <div>cccc</div>
       </div>
     </main>
   );
