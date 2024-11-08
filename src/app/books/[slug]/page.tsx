c0276fc165e288a9348e8d1af69305349982393b
uttamapaksa <uttamapaksa@gmail.com>
2024-11-08 17:52:21 +0900

    fix: slug params type promise

--- src/app/books/[slug]/page.tsx
+++ src/app/books/[slug]/page.tsx
@@ -1,4 +1,4 @@
-export default function AlgorithmDetail({ params }: { params: { slug: string } }) {
-  const slug = params.slug;
+export default async function AlgorithmDetail({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
   return <div>My Post: {slug}</div>;
 }
