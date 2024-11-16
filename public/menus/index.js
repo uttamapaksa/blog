bdcf1c8e772d287fd2c442e4c0951278f8342c8a
uttamapaksa <uttamapaksa@gmail.com>
2024-11-16 12:50:22 +0900

    refactor: from jpg to webp

--- public/menus/index.js
+++ public/menus/index.js
@@ -1,7 +1,7 @@
 // default as [menus.name]
 
-export { default as Books } from './jason-leung-D4YrzSwyIEc-unsplash.jpg';
-export { default as React } from './lautaro-andreani-xkBaqlcqeb4-unsplash.jpg';
-export { default as Algorithm } from './hans-isaacson-Goy1rUgU05I-unsplash.jpg';
-export { default as CS } from './nasa-Ed2AELHKYBw-unsplash.jpg';
-export { default as NotFound } from './muhammadh-saamy-Mv4r82sR-dA-unsplash.jpg';
+export { default as Books } from './jason-leung-D4YrzSwyIEc-unsplash.webp';
+export { default as React } from './lautaro-andreani-xkBaqlcqeb4-unsplash.webp';
+export { default as Algorithm } from './hans-isaacson-Goy1rUgU05I-unsplash.webp';
+export { default as CS } from './nasa-Ed2AELHKYBw-unsplash.webp';
+export { default as NotFound } from './muhammadh-saamy-Mv4r82sR-dA-unsplash.webp';
