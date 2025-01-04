63ac22ffde7283852de051b0c0eabef5cf251899
uttamapaksa <uttamapaksa@gmail.com>
2025-01-04 22:57:42 +0900

    refactor: rollback playground node

--- src/components/playground/playground-node.tsx
+++ src/components/playground/playground-node.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { ChevronRightIcon } from '@heroicons/react/20/solid';
 import { PlaygroundNodeType } from '@/lib/constants/types';
 import { playgroundNodes, playgroundPaths } from '@/lib/constants/playground';
-import { Disclosure, DisclosureButton, DisclosurePanel } from "@/components/custom-disclosure";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
 
 interface PlaygroundNodeProps {
   node: PlaygroundNodeType;
@@ -14,49 +13,38 @@ interface PlaygroundNodeProps {
 }
 
 export default function PlaygroundNode({ node, target, openSlugs }: PlaygroundNodeProps) {
-  const [first, setFirst] = useState(true);
   const nodePath = playgroundPaths[node.slug];
   const isTarget = target === node.slug;
   const isRoot = !nodePath.includes('/');
 
-  useEffect(() => {
-    setFirst(false);
-  }, []);
-
   return (
-    <Disclosure className="py-1 font-semibold tracking-tight">
-      {({ open, setOpen }) => {
-        if (first && !open && openSlugs.has(node.slug)) {
-          setOpen(true);
-        }
-        return (
-          node.childrenSlug.length > 0
-            ?
-            <>
-              <Link href={`/playground/${nodePath}`}>
-                <DisclosureButton setOpen={setOpen} className="group flex w-full items-center justify-between hover:underline hover:opacity-70">
-                  <span className={`py-1 ${isTarget ? 'text-blue-500' : isRoot ? '' : 'text-gray-700 dark:text-gray-400'}`}>
-                    {node.title}
-                  </span>
-                  <ChevronRightIcon className={`size-5 ${open && 'rotate-90'}`}/>
-                </DisclosureButton>
-              </Link>
-              <DisclosurePanel open={open} className="py-1 pl-6 pr-4 border-l border-gray-300 dark:border-gray-700">
-                {node.childrenSlug.map((childSlug) => (
-                  <PlaygroundNode key={childSlug} node={playgroundNodes[childSlug]} target={target} openSlugs={openSlugs} />
-                ))}
-              </DisclosurePanel>
-            </>
-            :
-            <>
-              <Link href={`/playground/${nodePath}`} className="flex w-full items-center justify-between hover:underline hover:opacity-70">
-                <span className={`py-1 ${isTarget ? 'text-blue-500' : isRoot ? '' : 'font-medium text-gray-700 dark:text-gray-400'}`}>
-                  {node.title}
-                </span>
-              </Link>
-            </>
-          )     
-        }}
+    <Disclosure as="div" className="py-1 font-semibold tracking-tight" defaultOpen={openSlugs.has(node.slug)}>
+      { node.childrenSlug.length > 0
+        ?
+        <>
+          <Link href={`/playground/${nodePath}`}>
+            <DisclosureButton className="group flex w-full items-center justify-between hover:underline hover:opacity-70">
+              <span className={`py-1 ${isTarget ? 'text-blue-500' : isRoot ? '' : 'text-gray-700 dark:text-gray-400'}`}>
+                {node.title}
+              </span>
+              <ChevronRightIcon className='size-5 group-data-[open]:rotate-90'/>
+            </DisclosureButton>
+          </Link>
+          <DisclosurePanel className="py-1 pl-6 pr-4 border-l border-gray-300 dark:border-gray-700">
+            {node.childrenSlug.map((childSlug) => (
+              <PlaygroundNode key={childSlug} node={playgroundNodes[childSlug]} target={target} openSlugs={openSlugs} />
+            ))}
+          </DisclosurePanel>
+        </>
+        :
+        <>
+          <Link href={`/playground/${nodePath}`} className="flex w-full items-center justify-between hover:underline hover:opacity-70">
+            <span className={`py-1 ${isTarget ? 'text-blue-500' : isRoot ? '' : 'font-medium text-gray-700 dark:text-gray-400'}`}>
+              {node.title}
+            </span>
+          </Link>
+        </>
+      }
     </Disclosure>
   );
 }
