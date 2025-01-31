9a4657ba653ee42af325ca6950d42f6932908b14
uttamapaksa <uttamapaksa@gmail.com>
2025-01-31 17:19:46 +0900

    docs: nextjs-blog 250131

--- src/app/[menu]/[slug]/page.tsx
+++ src/app/[menu]/[slug]/page.tsx
@@ -25,7 +25,7 @@ export default async function MenuSlugPage({ params }: MenuSlugPageProps) {
       <h1 className="mt-8 text-4xl font-semibold tracking-tight sm:text-5xl">{post.title}</h1>
       <p className="mt-6 text-lg sm:text-xl/8 text-gray-700 dark:text-gray-200">{post.summary}</p>
       <div className='flex flex-col items-center mt-6 sm:mt-12'>
-        <Image src={post.thumbnail} alt="post image" width={576} height={384} className="max-h-96 object-contain object-center" />
+        <Image src={post.thumbnail} alt="post image" width={576} height={384} className="max-h-96 object-contain" />
         <div className="flex justify-center mt-2"><span className='text-xs text-gray-500'>{post.source}</span></div>
       </div>
       <div className="prose max-w-full my-10 sm:my-20 dark:prose-invert dark:prose-h2:text-gray-100 dark:prose-h3:text-gray-100 dark:prose-h4:text-gray-100 dark:text-gray-100">
